24ee285694bf88c4a7662a20d69b956ec00b7efa
marco.coca <marco_cocaro@fet.edu.co>
2023-09-14 07:04:48 -0500

    nueva clase resultado

--- app/src/main/java/com/example/okfruitapp/resultado/ResultadoActivity.java
+++ app/src/main/java/com/example/okfruitapp/resultado/ResultadoActivity.java
@@ -3,5 +3,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.okfruitapp.R;
 public class ResultadoActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_resultado);
+    }
 }
