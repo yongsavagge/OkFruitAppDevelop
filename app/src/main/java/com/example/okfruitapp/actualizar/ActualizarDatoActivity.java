b26ef267cbc2c21ef964b0b4bc320d9f2a6b7ae8
marco.coca <marco_cocaro@fet.edu.co>
2023-09-20 14:09:56 -0500

    Clase ajuste

---
+++ app/src/main/java/com/example/okfruitapp/actualizar/ActualizarDatoActivity.java
@@ -0,0 +1,11 @@
+package com.example.okfruitapp.actualizar;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.okfruitapp.R;
+public class ActualizarDatoActivity  extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_actualizar_datos);
+    }
+}
