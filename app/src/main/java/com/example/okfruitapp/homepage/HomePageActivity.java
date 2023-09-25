c522c19d093e0a912244b4d54d6475ec78d7d333
lBrayanl <brayan_roasa@fet.edu.co>
2023-09-24 22:50:13 -0500

    Funcionalidad de botones Camara/Galeria

--- app/src/main/java/com/example/okfruitapp/homepage/HomePageActivity.java
+++ app/src/main/java/com/example/okfruitapp/homepage/HomePageActivity.java
@@ -1,15 +1,15 @@
 package com.example.okfruitapp.homepage;
-import android.os.Bundle;
-import android.widget.Button;
+
 import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-
-import com.example.okfruitapp.actualizar.ActualizarDatoActivity;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.okfruitapp.R;
+import com.example.okfruitapp.actualizar.ActualizarDatoActivity;
 import com.example.okfruitapp.resultado.ResultadoActivity;
 
 public class HomePageActivity extends AppCompatActivity {
@@ -18,16 +18,34 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         // Obtén una referencia al botón por su ID
+        Button btnDatos = findViewById(R.id.btn_dato);
         Button btnFotoSubir = findViewById(R.id.btn_foto);
         Button btnFotoTomar = findViewById(R.id.btn_foto2);
         com.google.android.material.floatingactionbutton.FloatingActionButton floatActualizar = findViewById(R.id.floatActualizar);
 
-        // Configura el evento de clic en el botón
-        btnFotoSubir.setOnClickListener(new View.OnClickListener() {
+        btnDatos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Crea un Intent para redireccionar a la otra actividad
                 Intent intent = new Intent(HomePageActivity.this, ResultadoActivity.class);
+                startActivity(intent);
+            }
+        });
+//
+        //btnFotoTomar.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        // Crea un Intent para redireccionar a la otra actividad
+        //        Intent intent = new Intent(HomePageActivity.this, ResultadoActivity.class);
+        //        startActivity(intent);
+        //    }
+        //});
+
+        btnFotoSubir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Abre la cámara
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(intent);
             }
         });
@@ -35,12 +53,11 @@ public class HomePageActivity extends AppCompatActivity {
         btnFotoTomar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Crea un Intent para redireccionar a la otra actividad
-                Intent intent = new Intent(HomePageActivity.this, ResultadoActivity.class);
+                // Abre la galería
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivity(intent);
             }
         });
-
         floatActualizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,5 +66,5 @@ public class HomePageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+        }
 }
